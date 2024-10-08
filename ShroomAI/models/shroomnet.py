1d8484758488dc00f397c8cb01317a56dba5fa8a
seohyeong <jseoh95@gmail.com>
2024-10-08 09:17:09 +0900

    ignore mobilevit training for now

--- ShroomAI/models/shroomnet.py
+++ ShroomAI/models/shroomnet.py
@@ -1,5 +1,17 @@
+import argparse
+import yaml
+
+import torch
 import torch.nn as nn
 from torchvision import models
+
+import sys
+sys.path.append('mlcvnets')
+# from mlcvnets.cvnets.models.classification.mobilevit_v2 import MobileViTv2
+# from mlcvnets.options.opts import get_training_arguments
+# from mlcvnets.cvnets import get_model
+# from mlcvnets.tests.configs import get_config
+# from mlcvnets.utils.common_utils import device_setup
 
 class ShroomNet(nn.Module):
     def __init__(self, num_classes, model_name):
@@ -11,6 +23,18 @@ class ShroomNet(nn.Module):
             self.model = models.mobilenet_v2(weights='IMAGENET1K_V1') # 3,504,872
         elif model_name == 'efficientnet_b0':
             self.model = models.efficientnet_b0(weights='IMAGENET1K_V1') # 5,288,548
+        # elif model_name == 'mobilevitv2-0.75':
+        #     # TODO: sort out path problem
+        #     config_file_path = "tests/data/datasets/classification/dummy_configs/image_classification_dataset.yaml"
+        #     opts = get_config(config_file=config_file_path)
+        #     # device set-up
+        #     opts = device_setup(opts)
+        #     with open('/home/user/seohyeong/ShroomAI/ShroomAI/models/mobilevitv2-0.75.yaml', 'r') as file:
+        #         data = yaml.safe_load(file)
+        #         opts = argparse.Namespace(**data)
+        #     self.model = get_model(opts)
+        #     # self.model = MobileViTv2('mobilevitv2-0.75.yaml')
+        #     # self.model = torch.load('mobilevitv2-0.75.pt')
             
         in_features = self.model.classifier[1].in_features
         self.model.classifier = nn.Sequential(
