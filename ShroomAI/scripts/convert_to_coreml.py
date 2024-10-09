86743b2188d45dafcf6a8585ea723e9daa9917d1
seohyeong <jseoh95@gmail.com>
2024-10-09 10:59:18 +0900

    clean up converting script

--- ShroomAI/scripts/convert_to_coreml.py
+++ ShroomAI/scripts/convert_to_coreml.py
@@ -1,46 +1,65 @@
-import torch
+import argparse
 import coremltools as ct
 import json
+import os
+import torch
 
 import sys
 sys.path.append('.')
 
 from ShroomAI.models.shroomnet import ShroomNet
 
-model_path = '/home/user/seohyeong/ShroomAI/ShroomAI/ckpt/mobilenet_v2_20241008_091337/mobilenet_v2_ft_ep30_bs256_lr1e-05.pth'
-label_map_path = '/home/user/seohyeong/ShroomAI/ShroomAI/ckpt/mobilenet_v2_20241008_091337/label_map.json'
+def main():
+    parser = argparse.ArgumentParser(description='Converting Torch Checkpoint to MLpackage Checkpoint')
 
-model = ShroomNet(num_classes=(1000, ), model_name='mobilenet_v2')
-checkpoint = torch.load(model_path, weights_only=True)
-model.load_state_dict(checkpoint['model_state_dict'])
-model.eval()
+    parser.add_argument('--model_path', type=str,
+                        default='/home/user/seohyeong/ShroomAI/ShroomAI/ckpt/mobilenet_v2_20241008_091337/mobilenet_v2_ft_ep30_bs256_lr1e-05.pth')
+    parser.add_argument('--label_map_path', type=str,
+                        default='/home/user/seohyeong/ShroomAI/ShroomAI/ckpt/mobilenet_v2_20241008_091337/label_map.json')
+    parser.add_argument('--mlpackage_save_name', type=str, default='mobilenet_v2.mlpackage')
+    parser.add_argument('--num_classes', type=int, default=1000)
+    parser.add_argument('--img_size', type=int, default=224)
+    parser.add_argument('--set_meta_data', action='store_true')
+    
+    args = parser.parse_args()
 
-example_input = torch.rand(1, 3, 224, 224) 
-traced_model = torch.jit.trace(model, example_input)
+    mlpackage_save_path = os.path.join(os.path.dirname(args.model_path), args.mlpackage_save_name)
+    
+    # load model
+    model = ShroomNet(num_classes=(args.num_classes, ), model_name='mobilenet_v2')
+    checkpoint = torch.load(args.model_path, weights_only=True)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.eval()
 
-with open(label_map_path) as f:
-    label_map = json.load(f)
+    example_input = torch.rand(1, 3, args.img_size, args.img_size) 
+    traced_model = torch.jit.trace(model, example_input)
 
-class_labels = list(label_map.keys())
+    # prepare class labels
+    with open(args.label_map_path) as f:
+        label_map = json.load(f)
+    class_labels = list(label_map.keys())
 
-# set config
-image_input = ct.ImageType(name='mobilenetv2_1.00_224_input', 
-                           shape=example_input.shape, # (1, 224, 224, 3,) 
-                           bias=[- 0.485/(0.229) , - 0.456/(0.224), - 0.406/(0.225)], 
-                           scale=1/(0.226*255.0))
-classifier_config = ct.ClassifierConfig(class_labels)
+    # convert the ckpt
+    image_input = ct.ImageType(name='mobilenetv2_1.00_224_input', 
+                            shape=example_input.shape,
+                            bias=[- 0.485/(0.229) , - 0.456/(0.224), - 0.406/(0.225)], 
+                            scale=1/(0.226*255.0))
+    classifier_config = ct.ClassifierConfig(class_labels)
 
-# convert
-mlmodel = ct.converters.convert(traced_model, 
-                                source='pytorch',
-                                convert_to='mlprogram',
-                                inputs=[image_input], 
-                                classifier_config=classifier_config)
+    mlmodel = ct.converters.convert(traced_model, 
+                                    source='pytorch',
+                                    convert_to='mlprogram',
+                                    inputs=[image_input], 
+                                    classifier_config=classifier_config)
 
-# set metadata
-mlmodel.author = 'Seohyeong Jeong'
-mlmodel.short_description = 'Mushroom Image Classification (currently 1,000 species supported, mobilenet_v2_ft_ep30_bs256_lr1e-05)'
-mlmodel.version = '1.0.0'
+    # set metadata
+    if args.set_meta_data:
+        mlmodel.author = 'Seohyeong Jeong'
+        mlmodel.short_description = 'Mushroom Image Classification (currently 1,000 species supported, mobilenet_v2_ft_ep30_bs256_lr1e-05)'
+        mlmodel.version = '1.0.0'
 
-# save mlmodel
-mlmodel.save('/home/user/seohyeong/ShroomAI/ShroomAI/ckpt/mobilenet_v2_20241008_091337/mobilenet_v2.mlpackage')
+    # save model
+    mlmodel.save(mlpackage_save_path)
+    
+if __name__=='__main__':
+    main()
